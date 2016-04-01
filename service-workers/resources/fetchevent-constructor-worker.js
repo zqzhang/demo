eba8d71804856d46998bb5ce1f7e5083ce74e5c3
chunywang <chunyanx.wang@intel.com>
2016-04-01 12:50:41 +0800

    Add permissions

--- service-workers/resources/fetchevent-constructor-worker.js
+++ service-workers/resources/fetchevent-constructor-worker.js
@@ -1,18 +1,21 @@
 importScripts("../../support/testharness.js");
 importScripts("../../support/testharnessreport.js");
 
+var url = new URL("blank-iframe.html", location).toString();
+var req = new Request(url);
+
 test(function() {
-  var event = new FetchEvent("FetchEvent");
+  var event = new FetchEvent("FetchEvent", {request: req});
   assert_equals(event.type, "FetchEvent", "FetchEvent type");
   assert_false(event.isReload);
-  assert_equals(event.request, null, "the initial request value");
+  assert_equals(event.request.url, url, "the initial request value");
+  assert_equals(event.clientId, null, "the initial clientId value");
 }, "FetchEvent constructor with type.");
 
 test(function() {
-  var url = new URL("blank-iframe.html", location).toString();
-  var req = new Request(url);
-  var event = new FetchEvent("FetchEvent", {request: req, isReload: true});
+  var event = new FetchEvent("FetchEvent", {request: req, isReload: true, clientId: "client-id-value"});
   assert_equals(event.type, "FetchEvent", "FetchEvent type");
-  assert_true(event.isReload, "the initial value is set true");
-  assert_equals(event.request.url, url, "the initial request value");
+  assert_true(event.isReload, "isReload is true");
+  assert_equals(event.request.url, url, "the request value");
+  assert_equals(event.clientId, "client-id-value", "the clientId value");  
 }, "FetchEvent constructor with initial FetchEventInit.");
